dc6be0f4714cce4288236464cea531b18af32dec
Szymon Staszkiewicz <simon@staszkiewicz.ca>
2024-07-06 14:34:40 -0700

    2 different handlers

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const buildClient = require('./lib/sigicom');
 const snsFailureSnsTopic = process.env.FAILURE_TOPIC_ARN;
 const AWS = require('aws-sdk');
 
-exports.handler = async (event) => {
+exports.c50Handler = async (event) => {
   const {
     baseUrl,
     username,
@@ -56,6 +56,73 @@ exports.handler = async (event) => {
     await csv.writeCsv(
       outfile,
       headers,
+      intervals,
+    );
+
+    console.log('Pushing to', remotePath);
+    await ftp.push(outfile, remotePath, ftpConfig);
+
+    fs.unlinkSync(outfile);
+
+  } catch(e) {
+    if (snsFailureSnsTopic) {
+      const params = {
+        Message: `Sigicom to instratus FTP sync failed for devices ${devices.map(d => d.serial).join(',')}: ${e}`,
+        TopicArn: snsFailureSnsTopic,
+      };
+      await new AWS.SNS({apiVersion: '2010-03-31'}).publish(params).promise();
+    }
+    throw e;
+  }
+}
+
+exports.handler = async (event) => {
+  const {
+    baseUrl,
+    username,
+    apiToken,
+    devices,
+    ftpHost,
+    ftpPass,
+    ftpPath,
+    ftpUser,
+    searchOffsetSeconds = 3600,
+    start,
+    end,
+  } = event;
+
+  try {
+    const sigicomClient = buildClient({
+      baseURL: baseUrl,
+      apiUser: username,
+      apiToken: apiToken,
+    });
+
+    const ftpConfig = {
+      host: ftpHost,
+      password: ftpPass,
+      user: ftpUser,
+    };
+
+    const to = end || Date.now();
+    const from = start || (to - (searchOffsetSeconds * 1000));
+    const searchData = await sigicomClient.getDataForDevices(
+      new Date(from),
+      new Date(to),
+      devices,
+      ["interval", "transient"]
+    );
+
+    const data = format.formatData(searchData);
+    const { intervals, transients } = data;
+
+    const timestamp = new Date().toISOString();
+    const outfile = `/tmp/out-${timestamp}.csv`;
+    const remotePath = path.join(ftpPath, `intervals-${Date.now()}.csv`);
+
+    await csv.writeCsv(
+      outfile,
+      Object.keys(intervals[0]).map(k => ({ id: k, title: k })),
       intervals,
     );
 
